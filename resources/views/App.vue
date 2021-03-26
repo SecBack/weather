f8b8aca6a815e1ffed29f5e3bbe5bca900406a4b
SecBack <albert3257@live.dk>
2021-03-26 14:42:22 +0100

    up until actually saving in db

---
+++ resources/views/App.vue
@@ -0,0 +1,38 @@
+<template>
+<div>
+  <b-button variant="outline-primary">Update database</b-button>
+
+  {{ data }}
+</div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      data: "",
+    }
+  },
+  mounted() {
+    axios.get("https://cors-anywhere.herokuapp.com/https://samples.openweathermap.org/data/2.5/box/city?bbox=12,32,15,37,10&appid=439d4b804bc8187953eb36d2a8c26a02")
+      .then((response) => {
+        this.data = response.data['list']
+      })
+
+    axios.post("api/data", {
+      name: "test",
+      temp: 9.002384,
+      prediction: "test prediction",
+      description: "test description",
+    })
+      .then((response) => {
+        console.log(response.data)
+      })
+  },
+  methodes() {
+
+  }
+}
+</script>
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('data', 'App\Http\Controllers\DataController')->only(['store']);
